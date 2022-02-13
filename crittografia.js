90680f992949581d8e50fb58837dd5890bf1b79f
ecoyocena <enricoa@outlook.it>
2022-02-13 20:53:28 +0100

    cifrario
    cifrario di cesare funzionante + animazioni

--- crittografia.js
+++ crittografia.js
@@ -1,34 +1,41 @@
+function Cifrato(testo, testoCifrato, chiave, decifra){
+  var c
+  if (decifra) {
+    c=parseInt(-chiave.value);
 
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  else {
+    c=parseInt(chiave.value);
 
-/* Gestisce l'input/output con la pagina web e richiama encrypt() */
-function encryptWeb() {
-  var msg = $('#decrypted-msg').val();  // Recupera il messaggio in chiaro
-  var key = parseInt($('#key').val());  // Recupera il valore della chiave
-  var result = encrypt(msg, key);  // Codifica il messaggio
-  $('#encrypted-msg').val(result);  // Mostra il testo nella pagina HTML
+  }
+  var alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  testo.value = testo.value.toUpperCase();
+
+  testoCifrato.value = "";
+
+  while (c<0) c += 26;
+
+  var len = testo.value.length;
+
+  for (var i = 0; i < len; i++){
+var ch = testo.value.charAt(i);
+
+if (ch >= 'A' & ch <= 'Z') {
+          
+    var pos = alfabeto.indexOf(ch);
+
+    pos = (pos + c) % 26;
+          
+    testoCifrato.value += alfabeto.charAt(pos);
+}
+  }
+}
+
+function Cancella(testo, testoCifrato, chiave){
+testo.value = "";
+testoCifrato.value = "";
+chiave.value = 3;
 }
 
 
-
-    /* Restituisce un messaggio codificato con uno spostamento di key posizioni */
-function encrypt(msg, key) {
-    var result = "";  // Inizializza il messaggio criptato
-    msg = msg.toUpperCase();  // Converte il messaggio in maiuscolo
-  
-    for (var i = 0; i < msg.length; i++) {  // Scorre i caratteri del messaggio
-      var char = msg.charAt(i);  // Carattere i-esimo
-  
-      if (alphabet.indexOf(char) >= 0) {  // Controlla se è una lettera
-        var index = char.charCodeAt(0) + key;  // Indice del carattere  cifrato
-  
-        if (index > 'Z'.charCodeAt(0)) {  // Se l'indice va oltre 'Z'
-          index = index - 26;  // Riparti in modo circolare da 'A'
-        }
-        result += String.fromCharCode(index);  // + carattere calcolato
-      } else {
-        result += char;  // Copia i caratteri che non sono lettere
-      }
-    }
-    return result;
-  }
+//alcune parti dello script sono state gentilmente prese in prestito da https://www.codingcreativo.it/cifrario-di-cesare-online  
