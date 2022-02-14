a2f4d4a877618f7a0b9e4f5d5fa85519570dd0d5
ecoyocena <enricoa@outlook.it>
2022-02-14 18:40:31 +0100

    migliorie
    codice indentato decentemente, progress bar e prima parte della home

---
+++ script/crittografia.js
@@ -0,0 +1,73 @@
+function Cifrato(testo, testoCifrato, chiave, decifra) {
+	var c
+
+	Pulitore();
+
+	if (testo.value == "") {
+		document.getElementById("testo").style.background = '#e81717';
+		document.getElementById("testo").style.border = 'none';
+		document.getElementById("displayato_testo").style.display = 'contents';
+
+	}
+
+	if (decifra) {
+		c = parseInt(-chiave.value);
+
+	} else {
+		c = parseInt(chiave.value);
+
+	}
+
+	if (c == 0) {
+		alert("ATTENZIONE!! Mettendo la chiave uguale a 0 ti verrà restituito lo stesso testo");
+
+	} else if (c > 26) {
+		document.getElementById("chiavee").style.background = '#e81717';
+		document.getElementById("chiavee").style.border = 'none';
+		document.getElementById("displayato").style.display = 'contents';
+		return;
+	} else {
+		var alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+		testo.value = testo.value.toUpperCase();
+
+		testoCifrato.value = "";
+
+		while (c < 0) c += 26;
+
+		var len = testo.value.length;
+
+		for (var i = 0; i < len; i++) {
+			var ch = testo.value.charAt(i);
+
+			if (ch >= 'A' & ch <= 'Z') {
+
+				var pos = alfabeto.indexOf(ch);
+
+				pos = (pos + c) % 26;
+
+				testoCifrato.value += alfabeto.charAt(pos);
+			}
+		}
+
+	}
+}
+
+function Cancella(testo, testoCifrato, chiave) {
+	testo.value = "";
+	testoCifrato.value = "";
+	chiave.value = "";
+	Pulitore();
+}
+
+function Pulitore() {
+	document.getElementById("chiavee").style.background = 'white';
+	document.getElementById("chiavee").style.border = 'solid 1px #767676';
+	document.getElementById("displayato").style.display = 'none';
+	document.getElementById("testo").style.background = 'white';
+	document.getElementById("testo").style.border = 'solid 1px #767676';
+	document.getElementById("displayato_testo").style.display = 'none';
+}
+
+
+//alcune parti dello script sono state gentilmente prese in prestito da https://www.codingcreativo.it/cifrario-di-cesare-online
---
+++ script/script.js
@@ -0,0 +1,39 @@
+function oro() {
+
+	var a = new Date();
+	var b = a.getHours();
+	var c = a.getMinutes();
+	var d = a.getSeconds();
+
+
+	if (b < 10 && c < 10 && d < 10) {
+		document.getElementById("scr").innerHTML = ("0" + b + ":0" + c + ":0" + d);
+	} else if (d < 10 && c < 10) {
+		document.getElementById("scr").innerHTML = (b + ":0" + c + ":0" + d);
+	} else if (b < 10 && c < 10) {
+		document.getElementById("scr").innerHTML = ("0" + b + ":0" + c + ":" + d);
+	} else if (b < 10 && d < 10) {
+		document.getElementById("scr").innerHTML = ("0" + b + ":" + c + ":0" + d);
+	} else if (d < 10) {
+		document.getElementById("scr").innerHTML = (b + ":" + c + ":0" + d);
+	} else if (c < 10) {
+		document.getElementById("scr").innerHTML = (b + ":0" + c + ":" + d);
+	} else if (b < 10) {
+		document.getElementById("scr").innerHTML = ("0" + b + ":" + c + ":" + d);
+	} else {
+		document.getElementById("scr").innerHTML = (b + ":" + c + ":" + d);
+	}
+
+}
+setInterval(oro, 1000);
+
+window.onscroll = function () {
+	myFunction()
+};
+
+function myFunction() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("barra-progressi").style.width = scrolled + "%";
+}
