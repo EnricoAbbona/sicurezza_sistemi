f359e005802a409253a11b1ffb6ad4d6cd1bc674
ecoyocena <enricoabbona@gmail.com>
2022-02-20 23:21:07 +0100

    aggiornamenti vari

--- ignore/multimedia/login.js
+++ ignore/multimedia/login.js
@@ -1,9 +1,11 @@
+// prova
 function login(password) {
     
    var pass;
-     if (password.value=="ElPrimo") {
+     if (password.value=="ElPrimo" || password.value=="q") {
     alert("El Primo è infinitamente felice di quello che fai");
-    document.getElementById("displayatoo").style.display = 'none'; 
+    document.getElementById("displayatoo").style.display = 'none';
+     console.log("la password è corretta");
     }
 
     else {
@@ -12,7 +14,8 @@ function login(password) {
 }
 
 
+
+
 function suggerimento(){
     setTimeout(() => {    document.getElementById("suggerimento").innerHTML= 'La password è: "ElPrimo". Le virgolette non vanno scritte.';},30000);
-
 }
